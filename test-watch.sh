6353cfa961b73e681816a13fb809ddddc6724ae8
Robin Blomberg <hello@robinblomberg.se>
2022-03-27 13:16:35 +0200

    build: remove unused Node flag

--- test-watch.sh
+++ test-watch.sh
@@ -1,1 +1,1 @@
-NODE_NO_WARNINGS=1 ts-node-dev --experimental-fetch --quiet --require tsconfig-paths/register --respawn ./src/index.test.ts
+NODE_NO_WARNINGS=1 ts-node-dev --quiet --require tsconfig-paths/register --respawn ./src/index.test.ts
